26454a1b922c2086c7f8f8f93309f0964c7d575a
Tiara <tinywomanintech@gmail.com>
2021-07-26 14:58:58 +0700

    done

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -6,30 +6,6 @@ import { heroLego } from '../assets/images/index.js';
 
 export default function ComingSoon() {
   return (
-    // <section className="comingsoon-section">
-    //   <Container className="comingsoon-container">
-    //     <div className="d-flex align-items-center text-center   ">
-    //       <div>
-    //         <div className="mx-auto text-dark">
-    //           <div className="row align-items-center ">
-    //             <div className="cs-image">
-    //               <img src={heroLego} width="184" height="305" alt="bitmap" />
-    //               <br />
-    //               <h1>Coming Soon!</h1>
-    //               <Link to="/" className="text-dark font-weight-medium">
-    //                 Back to home
-    //               </Link>
-    //             </div>
-    //           </div>
-    //           <div>
-    //             <div></div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </Container>
-    // </section>
-
     <section className="comingsoon-section">
       <Container className="comingsoon-container">
         <div className="comingsoon-content">
