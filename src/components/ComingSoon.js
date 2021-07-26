16187b4b0e4bb993aded02b6aa72172a99f8767d
Tiara <tinywomanintech@gmail.com>
2021-07-26 14:53:21 +0700

    added coming soon page

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -2,28 +2,42 @@ import React from 'react';
 import './ComingSoon.scss';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { heroLego } from '../assets/images/index.js';
 
 export default function ComingSoon() {
   return (
-    <section>
-      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '1440px', height: '100%' }}>
-        <div className="d-flex align-items-center text-center error-page  pt-5 pb-4 h-100">
-          <div className="row flex-grow">
-            <div className="col-lg-8 mx-auto text-dark">
-              <div className="row align-items-center ">
-                <div>
-                  <h2>Coming soon!</h2>
-                </div>
-              </div>
-              <div className="row mt-5">
-                <div className="col-12 text-center mt-xl-2">
-                  <Link to="/" className="text-dark font-weight-medium">
-                    Back to home
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+    // <section className="comingsoon-section">
+    //   <Container className="comingsoon-container">
+    //     <div className="d-flex align-items-center text-center   ">
+    //       <div>
+    //         <div className="mx-auto text-dark">
+    //           <div className="row align-items-center ">
+    //             <div className="cs-image">
+    //               <img src={heroLego} width="184" height="305" alt="bitmap" />
+    //               <br />
+    //               <h1>Coming Soon!</h1>
+    //               <Link to="/" className="text-dark font-weight-medium">
+    //                 Back to home
+    //               </Link>
+    //             </div>
+    //           </div>
+    //           <div>
+    //             <div></div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Container>
+    // </section>
+
+    <section className="comingsoon-section">
+      <Container className="comingsoon-container">
+        <div className="comingsoon-content">
+          <img src={heroLego} width="184" height="305" alt="bitmap" />
+          <h1>Coming Soon!</h1>
+          <Link to="/" className="text-dark font-weight-medium">
+            Back to home
+          </Link>
         </div>
       </Container>
     </section>
